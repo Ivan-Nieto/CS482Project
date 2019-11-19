9c1c1aaba5201997449bad55f1dfe4c023b38d9b
acruz42 <adorian@nmsu.edu>
2019-11-19 16:03:33 -0700

    further fixed formatting

--- ProjectPhase2.py
+++ ProjectPhase2.py
@@ -155,21 +155,21 @@ def retrieve(tableName):
       rows = cursor.fetchall()
       desc = cursor.description
       if tableName.lower() == "players":
-         output = ("{0:>0} {1:>10} {2:>12} {3:>8} {4:>12} {5:>12} {6:>12} {7:>10}".format(desc[0][0], desc[1][0], desc[2][0], desc[3][0], desc[4][0], desc[5][0], desc[6][0], desc[7][0])) + "\n"
+         output = ("{0:>8} {1:>10} {2:>15} {3:>8} {4:>12} {5:>12} {6:>12} {7:>10}".format(desc[0][0], desc[1][0], desc[2][0], desc[3][0], desc[4][0], desc[5][0], desc[6][0], desc[7][0])) + "\n"
          for row in rows:
-            output = output + ("{0:>0} {1:>10} {2:>15} {3:>8} {4:>12} {5:>12} {6:>12} {7:>10}".format(row[0], row[1], row[2], row [3], row[4], row[5], row[6], row[7])) + "\n"
+            output = output + ("{0:>8} {1:>10} {2:>15} {3:>8} {4:>12} {5:>12} {6:>12} {7:>10}".format(row[0], row[1], row[2], str(row[3]), row[4], row[5], row[6], row[7])) + "\n"
       elif (tableName.lower() == "games"):
-         output = ("{0:>0} {1:>10} {2:>35} {3:>10} {4:>15} {5:>15}".format(desc[0][0], desc[1][0], desc[2][0], desc[3][0], desc[4][0], desc[5][0])) + "\n"
+         output = ("{0:>8} {1:>10} {2:>20} {3:>10} {4:>15} {5:>15}".format(desc[0][0], desc[1][0], desc[2][0], desc[3][0], desc[4][0], desc[5][0])) + "\n"
          for row in rows:
-            output = output + ("{0:>0} {1:} {2:>35} {3:>10} {4:>15} {5:>15}".format(row[0], row[1], row[2], row [3], row[4], row[5])) + "\n"
+            output = output + ("{0:>8} {1:>10} {2:>20} {3:>10} {4:>15} {5:>15}".format(row[0], str(row[1]), row[2], row [3], row[4], row[5])) + "\n"
       elif (tableName.lower() == "teams"):
-         output = ("{0:>0} {1:>12} {2:>15}".format(desc[0][0], desc[1][0], desc[2][0])) + "\n"
+         output = ("{0:>8} {1:>12} {2:>15}".format(desc[0][0], desc[1][0], desc[2][0])) + "\n"
          for row in rows:
-            output = output + ("{0:>0} {1:>15} {2:>15} ".format(row[0], row[1], row[2])) + "\n"
+            output = output + ("{0:>8} {1:>12} {2:>15} ".format(row[0], row[1], row[2])) + "\n"
       elif (tableName.lower() == "play"):
-         output = ("{0:>0} {1:>7}".format(desc[0][0], desc[1][0])) + "\n"
+         output = ("{0:>8} {1:>10}".format(desc[0][0], desc[1][0])) + "\n"
          for row in rows:
-            output = output + ("{0:>0} {1:>10}".format(row[0], row[1])) + "\n"
+            output = output + ("{0:>8} {1:>10}".format(row[0], row[1])) + "\n"
       cursor.close()
       connection.commit()
    except Exception as e:
